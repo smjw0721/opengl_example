e243b52509af3aeaeec04c586d93c64f6ed8cc87
jinwoo <smjw0721@gmail.com>
2021-04-16 23:58:05 +0900

    week_7

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,13 @@
 #include <spdlog/spdlog.h>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+
 
         
-void OnKeyEvent(GLFWwindow* window,
-    int key, int scancode, int action, int mods) {
+void OnKeyEvent(GLFWwindow* window,int key, int scancode, int action, int mods) {
+    ImGui_ImplGlfw_KeyCallback(window, key, scancode, action, mods);
     SPDLOG_INFO("key: {}, scancode: {}, action: {}, mods: {}{}{}",
         key, scancode,
         action == GLFW_PRESS ? "Pressed" :
@@ -18,9 +21,33 @@ void OnKeyEvent(GLFWwindow* window,
         glfwSetWindowShouldClose(window, true);
     }
 }
+
+void OnCursorPos(GLFWwindow* window, double x, double y) {
+  auto context = (Context*)glfwGetWindowUserPointer(window);
+  context->MouseMove(x, y);
+}
+
+void OnMouseButton(GLFWwindow* window, int button, int action, int modifier) {
+    ImGui_ImplGlfw_MouseButtonCallback(window, button, action, modifier);
+    auto context = (Context*)glfwGetWindowUserPointer(window);
+    double x, y;
+    glfwGetCursorPos(window, &x, &y);
+    context->MouseButton(button, action, x, y);
+}
+
+void OnCharEvent(GLFWwindow* window, unsigned int ch) {
+    ImGui_ImplGlfw_CharCallback(window, ch);
+}
+
+void OnScroll(GLFWwindow* window, double xoffset, double yoffset) {
+    ImGui_ImplGlfw_ScrollCallback(window, xoffset, yoffset);
+}
+
+
 void OnFramebufferSizeChange(GLFWwindow* window, int width, int height) {
     SPDLOG_INFO("framebuffer size changed: ({} x {})", width, height);
-    glViewport(0, 0, width, height);
+    auto context = reinterpret_cast<Context*>(glfwGetWindowUserPointer(window));
+    context->Reshape(width, height);
 } 
 
 int main(int argc, const char** argv){
@@ -59,12 +86,20 @@ if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
 auto glVersion = glGetString(GL_VERSION);
 SPDLOG_INFO("OpenGL context version: {}", glVersion);
 
+    auto imguiContext = ImGui::CreateContext();
+    ImGui::SetCurrentContext(imguiContext);
+    ImGui_ImplGlfw_InitForOpenGL(window, false);
+    ImGui_ImplOpenGL3_Init();
+    ImGui_ImplOpenGL3_CreateFontsTexture();
+    ImGui_ImplOpenGL3_CreateDeviceObjects();
+
 auto context = Context::Create();
 if (!context) {
   SPDLOG_ERROR("failed to create context");
   glfwTerminate();
   return -1;
 }
+glfwSetWindowUserPointer(window, context.get());
 
 ShaderPtr vertShader = Shader::CreateFromFile("./shader/simple.vs", GL_VERTEX_SHADER);
 ShaderPtr fragShader = Shader::CreateFromFile("./shader/simple.fs", GL_FRAGMENT_SHADER);
@@ -77,15 +112,31 @@ SPDLOG_INFO("program id: {}", program->Get());
     OnFramebufferSizeChange(window, WINDOW_WIDTH, WINDOW_HEIGHT);   
     glfwSetFramebufferSizeCallback(window,OnFramebufferSizeChange);
     glfwSetKeyCallback(window,OnKeyEvent);
+    glfwSetCharCallback(window, OnCharEvent);
+    glfwSetCursorPosCallback(window, OnCursorPos);  	
+    glfwSetMouseButtonCallback(window, OnMouseButton);
+    glfwSetScrollCallback(window, OnScroll);
 
     // glfw 루프 실행, 윈도우 close 버튼을 누르면 정상 종료
     SPDLOG_INFO("Start main loop");
     while (!glfwWindowShouldClose(window)) {
-       glfwPollEvents();
+        glfwPollEvents();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        context->ProcessInput(window);
         context->Render();
+        ImGui::Render();
+
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
         glfwSwapBuffers(window);
     }
     context.reset();
+
+        ImGui_ImplOpenGL3_DestroyFontsTexture();
+        ImGui_ImplOpenGL3_DestroyDeviceObjects();
+        ImGui_ImplGlfw_Shutdown();
+        ImGui::DestroyContext(imguiContext);
 
     glfwTerminate();
     return 0;
